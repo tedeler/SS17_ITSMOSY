4058a65ba172e98f7b6575f4054979d81aca1ea0
Andreas Plaß <andreas.plass@haw-hamburg.de>
2017-03-21 15:58:28 +0100

    class Square

--- ms-javascript/canvas/code.js
+++ ms-javascript/canvas/code.js
@@ -2,7 +2,39 @@ function initialize(){
 	const canvas = document.getElementById("graphics");
 	const ctx = canvas.getContext("2d");
 	
-
+	ctx.fillStyle = '#FFFF00';
+	ctx.fillRect(50, 100, 380, 400);
+	ctx.fillStyle = 'rgba(0, 0, 128, 0.8)';
+	ctx.fillRect(25, 50, 70, 190);
+	
+	class Square{
+		constructor(x, y, width){
+			this.x = x;
+			this.y = y; 
+			this.width = width;
+		}
+		draw(context){
+			context.fillRect(this.x, this.y, 
+							this.width, this.width);
+		}
+	}
+	let square = new Square(10, 10, 30);
+	square.x = 200;
+	square.draw(ctx);
+	
+	class ColoredSquare extends Square{
+		constructor(x, y, width, color){
+			super(x, y, width);
+			this.fillStyle = color;
+		}
+		draw(context){
+			context.fillStyle = this.fillStyle;
+			super.draw(context);
+		}
+	}
+	let magentaSquare 
+				= new ColoredSquare(20, 40, 70, "magenta");
+	magentaSquare.draw(ctx);
 }
 
 
