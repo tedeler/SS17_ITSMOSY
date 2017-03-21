8422e51a26b7fbdee0ebe735368c790b7951e7e7
Andreas Plaß <andreas.plass@haw-hamburg.de>
2017-03-21 17:00:35 +0100

    rgab

--- ms-javascript/rgb/code.js
+++ ms-javascript/rgb/code.js
@@ -2,27 +2,28 @@ function initialize(){
 	// define event listener
 	function onSliderChanged(){
 		// get the R, G, B values from the slider
-		var slider = document.getElementById("slider-red");
-		var red = slider.value;
+		let slider = document.getElementById("slider-red");
+		let red = slider.value;
 		
 		slider = document.getElementById("slider-green");
-		var green = slider.value;
+		let green = slider.value;
 
 		slider = document.getElementById("slider-blue");
-		var blue = slider.value;
+		let blue = slider.value;
 		
 		// set the background color of the body tag 
-		var body = document.getElementById("body");
+		let body = document.getElementById("body");
 		body.style["background-color"] 
-				= "rgb(" + red + "," + green + "," + blue + ")";
+				= `rgb(${red}, ${green}, ${blue})`;
+// oder:		= "rgb(" + red + "," + green + "," + blue + ")";
 	}
 	
 	// set event listener
-	var redSlider = document.getElementById("slider-red");
+	let redSlider = document.getElementById("slider-red");
 	redSlider.addEventListener("input", onSliderChanged);
-	var greenSlider = document.getElementById("slider-green");
+	let greenSlider = document.getElementById("slider-green");
 	greenSlider.addEventListener("input", onSliderChanged);
-	var blueSlider = document.getElementById("slider-blue");
+	let blueSlider = document.getElementById("slider-blue");
 	blueSlider.addEventListener("input", onSliderChanged);
 	// initialize the color
 	onSliderChanged();
